724be1c2f51b73dd11d44375d9c530d36e15835c
djamelkorei <korei.djamel.eddine@gmail.com>
2023-10-12 22:17:21 +0100

    initial commit

---
+++ src/main/java/com/nonestack/springbootgetstarted/SpringBootGetStartedApplication.java
@@ -0,0 +1,13 @@
+package com.nonestack.springbootgetstarted;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGetStartedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootGetStartedApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nonestack/springbootgetstarted/SpringBootGetStartedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nonestack.springbootgetstarted;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootGetStartedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
