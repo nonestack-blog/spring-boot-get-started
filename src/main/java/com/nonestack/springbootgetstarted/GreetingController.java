2dbcb5ecf8d6f9039a4d5b613949ecd0f43dd1c3
djamelkorei <korei.djamel.eddine@gmail.com>
2023-10-13 23:41:22 +0100

    web application

---
+++ src/main/java/com/nonestack/springbootgetstarted/GreetingController.java
@@ -0,0 +1,14 @@
+package com.nonestack.springbootgetstarted;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+	@GetMapping("/")
+	public String index() {
+		return "Greetings from Nonestack!";
+	}
+
+}
